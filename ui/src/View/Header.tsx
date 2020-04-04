9869c0567ee0aeb49f6048544858342c79ec1a64
ShuzoN <d.namu.r21@gmail.com>
2020-04-04 19:01:42 +0900

    headerにロゴを追加

--- ui/src/View/Header.tsx
+++ ui/src/View/Header.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Button, } from '@material-ui/core';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyle = makeStyles((theme) => ({
+  logo: {
+    maxHeight: "40px",
+  },
+  logoBlock: { margin: "0 auto" },
+  appBar: {
+    background: "#fff",
+  },
+}));
 
 export const Header = (): JSX.Element => {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    href="/new"
-                >
-                    <NoteAddIcon />
-                </IconButton>
-                <Button size="large" variant="text" color="inherit" href="/">
-                    Todos
-                </Button>
-            </Toolbar>
-        </AppBar >
-    );
-}
+  const c = useStyle();
 
+  return (
+    <AppBar position="static" className={c.appBar}>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon color="primary" />
+        </IconButton>
+        <div className={c.logoBlock}>
+          <Button size="large" variant="text" color="inherit" href="/">
+            <img
+              src="/img/headphonista_logo.png"
+              alt="Logo"
+              className={c.logo}
+            />
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
