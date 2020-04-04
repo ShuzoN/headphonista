d560e0dc0adb0331258fe0ee45867ca22353d0b4
ShuzoN <d.namu.r21@gmail.com>
2020-04-04 19:53:51 +0900

    sideMenu

--- ui/src/View/Header.tsx
+++ ui/src/View/Header.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
   IconButton,
   Button,
   makeStyles,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import TrainIcon from "@material-ui/icons/Train";
+import ComputerIcon from "@material-ui/icons/Computer";
+import SportsHandballIcon from "@material-ui/icons/SportsHandball";
+import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
+import BatteryChargingFullIcon from "@material-ui/icons/BatteryChargingFull";
 
 const useStyle = makeStyles((theme) => ({
   logo: {
@@ -24,9 +35,7 @@ export const Header = (): JSX.Element => {
   return (
     <AppBar position="static" className={c.appBar}>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon color="primary" />
-        </IconButton>
+        <MenuBar />
         <div className={c.logoBlock}>
           <Button size="large" variant="text" color="inherit" href="/">
             <img
@@ -40,3 +49,76 @@ export const Header = (): JSX.Element => {
     </AppBar>
   );
 };
+
+enum Menus {
+  Commuting = "通勤通学用",
+  Work = "作業用",
+  Sports = "スポーツ",
+  Iphone = "iPhoneユーザ",
+  LongBattery = "とにかく充電持ちがいい",
+}
+
+function MenuBar(): JSX.Element {
+  const [openState, setOpenState] = useState(false);
+
+  const list = (
+    <List>
+      <ListItem>
+        <Typography variant="caption">使い方を選んでください</Typography>
+      </ListItem>
+      <ListItem button key={Menus.Commuting}>
+        <ListItemIcon>
+          <TrainIcon />
+        </ListItemIcon>
+        <ListItemText primary={Menus.Commuting} />
+      </ListItem>
+      <ListItem button key={Menus.Work}>
+        <ListItemIcon>
+          <ComputerIcon />
+        </ListItemIcon>
+        <ListItemText primary={Menus.Work} />
+      </ListItem>
+      <ListItem button key={Menus.Sports}>
+        <ListItemIcon>
+          <SportsHandballIcon />
+        </ListItemIcon>
+        <ListItemText primary={Menus.Sports} />
+      </ListItem>
+      <ListItem button key={Menus.Iphone}>
+        <ListItemIcon>
+          <PhoneIphoneIcon />
+        </ListItemIcon>
+        <ListItemText primary={Menus.Iphone} />
+      </ListItem>
+      <ListItem button key={Menus.LongBattery}>
+        <ListItemIcon>
+          <BatteryChargingFullIcon />
+        </ListItemIcon>
+        <ListItemText primary={Menus.LongBattery} />
+      </ListItem>
+    </List>
+  );
+
+  const onClick = () => setOpenState(!openState);
+
+  return (
+    <>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={() => onClick()}
+      >
+        <MenuIcon color="primary" />
+      </IconButton>
+      <SwipeableDrawer
+        anchor="left"
+        open={openState}
+        onClose={() => onClick()}
+        onOpen={() => onClick()}
+      >
+        {list}
+      </SwipeableDrawer>
+    </>
+  );
+}
