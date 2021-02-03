528ee90b16544b4af82d94301fce850ba6a4791e
ShuzoN <d.namu.r21@gmail.com>
2021-02-03 10:00:12 +0900

    item name

--- ui/src/View/Item/index.tsx
+++ ui/src/View/Item/index.tsx
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import { typography } from "material-ui/styles";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,22 +16,34 @@ const useStyles = makeStyles((theme: Theme) =>
         marginTop: theme.spacing(2),
       },
     },
+    itemName: {
+      margin: theme.spacing(0, 2),
+    },
   })
 );
 
 export const Item = (): JSX.Element => {
+  const c = useStyles();
+
   return (
     <>
       <ItemBreadcrumbs />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h6" align="left" className={c.itemName}>
+            SONY MDR-M1ST
+          </Typography>
+        </Grid>
+      </Grid>
     </>
   );
 };
 
 export default function ItemBreadcrumbs() {
-  const classes = useStyles();
+  const c = useStyles();
 
   return (
-    <div className={classes.breadcrumbs}>
+    <div className={c.breadcrumbs}>
       <Breadcrumbs separator="›" aria-label="breadcrumb">
         <Link color="inherit" href="/">
           <Typography variant="body2">商品一覧</Typography>
