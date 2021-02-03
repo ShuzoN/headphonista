184afe7a0c4153205c1f9a07fe624404322e7611
ShuzoN <d.namu.r21@gmail.com>
2021-02-03 09:29:08 +0900

    item init

---
+++ ui/src/View/Item/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({}));
+
+export const Item = (): JSX.Element => {
+  const c = useStyle();
+
+  return <></>;
+};
