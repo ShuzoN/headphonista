5970d8a31f2f4d3649cd356afe44f5b0d60e5928
ShuzoN <d.namu.r21@gmail.com>
2021-02-10 00:27:35 +0900

    chartに利用しない数字は消す

--- ui/src/View/Item/ItemRadarChart.tsx
+++ ui/src/View/Item/ItemRadarChart.tsx
@@ -23,8 +23,6 @@ const CHART_OPACITY = 0.6;
 export type ChartData = {
   subject: string;
   A: number;
-  B: number;
-  fullMark: number;
 };
 
 export const ItemRadarChart = (props: {
@@ -69,8 +67,6 @@ export function createChartData(
     return {
       subject: subject,
       A: value,
-      B: value,
-      fullMark: 150,
     };
   };
 
